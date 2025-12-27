0adbdae4ae7aa71f1fe7b30883206b57d5557733
Giorgi <vasokima@gmail.com>
2025-12-27 23:48:30 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = 'darka-hunter-v1';
+const CACHE_NAME = 'darka-life-v2';
 
-// ინსტალაციის დროს ქეშავს ძირითად ფაილებს
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-      ]);
+      return cache.addAll(['/', '/index.html']);
     })
   );
 });
 
-// მონაცემების წამოღება (Network First სტრატეგია - ჯერ ინტერნეტს ამოწმებს, რომ ლაივ ამინდი იყოს)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
